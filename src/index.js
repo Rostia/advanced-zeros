068ee734ad8767061d32052c80e5b08cc7161959
Rostia <habupuc@gmail.com>
2019-02-22 00:38:26 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var listResult = [];
+    var result = 0;
+    var listSimpleDevisor = simpleDevisor(base);
+    var copyNumber = number;
+    for(var key in listSimpleDevisor){
+        while(copyNumber>0){
+            copyNumber = Math.floor(copyNumber/key);
+            result += copyNumber;
+        }
+        listResult.push(Math.floor(result/listSimpleDevisor[key]));
+        copyNumber = number;
+        result = 0;
+    }
+    return listResult.sort((a, b) => a - b)[0];
+}
+function simpleDevisor(n){
+    var result = {};
+    var devisor = 2;
+    while(n !== 1){
+        if(Math.sqrt(n) < devisor) {
+            result[n] = (result[n] > 0) ? result[n]+1 : 1;
+            n = 1;
+        }
+        if(n%devisor === 0){
+            result[devisor] = (result[devisor] > 0) ? result[devisor]+1 : 1;
+            n /= devisor;
+            devisor = 2;
+            continue;
+        }
+        devisor++;
+    }
+    return result;
 }
